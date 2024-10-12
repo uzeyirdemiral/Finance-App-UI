41ba6019ee92135cccbc7c3cf56563aae43f7272
uzeyirdemiral <udemiral17@gmail.com>
2024-10-12 20:30:16 +0300

    added tab navigation

---
+++ app/(tabs)/transcations.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const transcations = () => {
+  return (
+    <View>
+      <Text>transcations</Text>
+    </View>
+  )
+}
+
+export default transcations
